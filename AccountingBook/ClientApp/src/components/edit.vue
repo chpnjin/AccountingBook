e834ef82d8401f1ff15c12994c4a8a33cad26705
Chen Jim <chpngen@gmail.com>
2019-08-09 11:42:03 +0800

    更新編輯交易頁面排版v1

--- AccountingBook/ClientApp/src/components/edit.vue
+++ AccountingBook/ClientApp/src/components/edit.vue
@@ -5,24 +5,37 @@
     <div id="flexBox">
       <fieldset id="main">
         <legend>主項目</legend>
-        <table border="1">
+        <div id="transactionInfo" class="mainItem">
+          <table border="1">
             <colgroup>
-            <col>
-            <col>
+              <col id="info_col1" />
+              <col id="info_col2" />
+              <col id="info_col3" />
+              <col id="info_col4" />
             </colgroup>
-          <tr>
-            <td>交易代碼</td>
-            <td><input type="text" id=""></td>
-          </tr>
-          <tr>
-            <td>交易日期</td>
-            <td><input type="date"></td>
-          </tr>
-          <tr>
-            <td>說明</td>
-            <td><input type="text"></td>
-          </tr>
-        </table>
+            <tr>
+              <td>交易代碼</td>
+              <td>
+                <input type="text" id />
+              </td>
+              <td>交易日期</td>
+              <td>
+                <input type="date" />
+              </td>
+            </tr>
+            <tr>
+              <td>說明</td>
+              <td colspan="3">
+                <input type="text" id="remark" />
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div id="editButtons" class="mainItem">
+          <input type="button" value="新增" />
+          <input type="button" value="更新" />
+          <input type="button" value="刪除" />
+        </div>
       </fieldset>
       <fieldset class="detial">
         <legend align="center">借方</legend>
@@ -50,17 +63,63 @@ module.exports = {
 </script>
 
 <style>
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
 #flexBox {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
 }
 
+td > input {
+  width: 100%;
+}
+
 #main {
+  display: flex;
   flex: 100%;
 }
 
 .detial {
   flex: 1;
+}
+
+#transactionInfo {
+  flex-grow: 10;
+  flex-shrink: 0;
+  flex-basis: 10px;
+}
+
+/* 交易主項目表格 */
+#transactionInfo table{
+  width: 100%;
+}
+
+#info_col1 {
+  width: 70px;
+}
+
+#info_col2{
+  width: 480px;
+}
+
+#info_col3 {
+  width: 70px;
+}
+
+#remark {
+  width: 100%;
+}
+/* 交易操作按鈕 */
+#editButtons {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-shrink: 0;
+  flex-basis: 10px;
 }
 </style>
--- AccountingBook/ClientApp/src/index.vue
+++ AccountingBook/ClientApp/src/index.vue
@@ -24,6 +24,7 @@
 </template>
 
 <script>
+    let selfData;
     module.exports = {
         data: function () {
             return {
