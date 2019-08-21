a6bc12a381b98023bfef3a60c3dbb91f654497c1
Chen Jim <chpngen@gmail.com>
2019-08-22 05:32:15 +0800

    新增對應科目定義表操作API

--- AccountingBook/Controllers/AccountController.cs
+++ AccountingBook/Controllers/AccountController.cs
@@ -8,35 +8,35 @@ namespace AccountingBook.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class AccountController : ControllerBase
     {
-        // GET api/values
+        // GET api/Account
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/Account/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/Account
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/Account/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/Account/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
