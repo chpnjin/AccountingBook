c8ada122ceb6bfcba273fa9bb07a7b20467d2f8e
chpngen <chpngen@gmail.com>
2024-12-06 18:52:51 +0800

    更改Port對應，以避免衝突

---
+++ api/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace api.Controllers
+{
+    /// <summary>
+    /// 呼叫測試
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("API_test")]
+        public async Task<ActionResult<string>> API_test()
+        {
+            await Task.Delay(2000);
+            return "TEST OK";
+        }
+
+        [HttpGet]
+        [Route("GetData")]
+        public async Task<ActionResult<User>> GetUser()
+        {
+            await Task.Delay(2000);
+            return new User()
+            {
+                Id = 10,
+                Name = "Winner"
+            };
+        }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
