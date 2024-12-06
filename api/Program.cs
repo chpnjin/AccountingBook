4ddd15703f471b484f8aeb43d7a835eaf865c40f
chpngen <chpngen@gmail.com>
2024-12-07 02:04:24 +0800

    docker-compose 設定調教

--- api/Program.cs
+++ api/Program.cs
@@ -1,18 +1,30 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
-app.Run();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    //新增Swagger支援
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    //開發環境專用
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseRouting();
+    app.MapControllers();  //一定要加,否則API呼叫會回傳404
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
